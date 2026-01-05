df76481d068e9fb226b4d03b4f0d3118802833ba
lucas <luccaass96@gmail.com>
2026-01-05 16:15:21 -0300

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,24 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.front.mascotai',
-  appName: 'MascotAI',
-  webDir: 'dist',
-  plugins: {
-    GoogleAuth: {
-      scopes: ['profile', 'email'],
-      androidClientId: '412244128184-507a1mt5t8lqtr0ce6sajpofb7akod2c.apps.googleusercontent.com',
-      serverClientId: '412244128184-507a1mt5t8lqtr0ce6sajpofb7akod2c.apps.googleusercontent.com',
-      forceCodeForRefreshToken: true,
-    },
-    CapacitorCookies: {
-      enabled: true, // Ya lo tenías, mantenelo así
-    },
+  "appId": "com.front.mascotai",
+  "appName": "MascotAI",
+  "webDir": "dist",
+  "server": {
+    "androidScheme": "https",
+    "hostname": "localhost"
   },
+  "plugins": {
+    "GoogleAuth": {
+      "scopes": ["profile", "email"],
+      "androidClientId": "412244128184-507a1mt5t8lqtr0ce6sajpofb7akod2c.apps.googleusercontent.com",
+      "serverClientId": "412244128184-507a1mt5t8lqtr0ce6sajpofb7akod2c.apps.googleusercontent.com",
+      "forceCodeForRefreshToken": true
+    },
+    "CapacitorCookies": {
+      "enabled": true
+    }
+  }
 };
+
+export default config;
